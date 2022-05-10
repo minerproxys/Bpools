07f0889623c30f76ff7d1099ef2c7d8a28644444
OnlyU <97244354+minerproxys@users.noreply.github.com>
2022-05-10 16:01:45 +0800

    Add files via upload

---
+++ Bpool反水器forWindows+各家破解文件/Win守护方式开启minerProxy_v4.0.0T9_windows_amd64.bat
@@ -0,0 +1,5 @@
+title "minerProxy_v4.0.0T9_windows_amd64.exe"
+:start
+minerProxy_v4.0.0T9_windows_amd64.exe
+goto start
+pause
---
+++ Bpool反水器forWindows+各家破解文件/Win守护方式开启反水器.bat
@@ -0,0 +1,5 @@
+title "作者反水器_V4.0 Bpools"
+:start
+Bpools_windows.exe
+goto start
+pause
