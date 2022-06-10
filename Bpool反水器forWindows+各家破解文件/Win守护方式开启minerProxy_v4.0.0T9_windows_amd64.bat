90342bdfd07796a5e83d68170974da4ad86a76d4
OnlyU <97244354+minerproxys@users.noreply.github.com>
2022-06-11 00:43:57 +0800

    Delete Bpool反水器forWindows+各家破解文件 directory

--- Bpool反水器forWindows+各家破解文件/Win守护方式开启minerProxy_v4.0.0T9_windows_amd64.bat
+++
@@ -1,5 +0,0 @@
-title "minerProxy_v4.0.0T9_windows_amd64.exe"
-:start
-minerProxy_v4.0.0T9_windows_amd64.exe
-goto start
-pause
--- Bpool反水器forWindows+各家破解文件/Win守护方式开启反水器.bat
+++
@@ -1,5 +0,0 @@
-title "作者反水器_V4.0 Bpools"
-:start
-Bpools_windows.exe
-goto start
-pause
